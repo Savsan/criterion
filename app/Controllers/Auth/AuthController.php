b6f1ea4f7765cdf98ef26b43a490e7f30a28fd7b
Alexandr Varabyou <vorobev.profi@gmail.com>
2016-07-28 16:56:17 +0300

    add companies to signUP page and auth controller

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Auth;
 
 use App\Controllers\Controller;
 use App\Models\User;
+use App\Models\Company;
 use Respect\Validation\Validator as v;
 
 class AuthController extends Controller{
@@ -50,7 +51,10 @@ class AuthController extends Controller{
 
 	public function getSignUp($request, $response)
 	{
-		return $this->view->render($response, 'auth/signup.twig');
+		$companies = Company::all();
+		return $this->view->render($response, 'auth/signup.twig', [
+			'companies' => $companies,
+		]);
 	}
 
 	public function postSignUp($request, $response)
@@ -64,6 +68,7 @@ class AuthController extends Controller{
 			'email' => v::noWhitespace()->notEmpty()->EmailAvailible(),
 			'password' => v::noWhitespace()->notEmpty(),
 			'role' => v::noWhitespace()->notEmpty(),
+			'users_company' => v::noWhitespace()->notEmpty(),
 
 		]);
 
